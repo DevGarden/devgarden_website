38262c258dc7d63bebadc0a7c3ba46fbb9ea0c0a
Nick Roberts <nick.roberts@utah.edu>
2016-03-04 12:15:42 -0700

    updated create user sql to create user if not exists

--- config/create_db_user.sql
+++ config/create_db_user.sql
@@ -1,5 +1,4 @@
-create user 'devgarden_web'@'localhost' IDENTIFIED BY 'devgarden_web';
-grant all on devgarden_web_test.* to 'devgarden_web'@'localhost';
-grant all on devgarden_web_development.* to 'devgarden_web'@'localhost';
+grant all on devgarden_web_test.* to 'devgarden_web'@'localhost' IDENTIFIED BY 'devgarden_web';
+grant all on devgarden_web_development.* to 'devgarden_web'@'localhost' IDENTIFIED BY 'devgarden_web';
 flush privileges;
 
