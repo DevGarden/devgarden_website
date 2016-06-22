d6118cb8520ef58399219b62d6645acf54632455
trentonsnyder <trenton.m.snyder@gmail.com>
2016-06-22 12:15:56 -0600

    portfolio click on landing, sigin in reintegrated

---
+++ app/assets/javascripts/portfolio.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  $('#iot').click(
+    function() {
+      $('.project-circle').removeClass('hide-circle');
+      $('.project-circle').addClass('animated fadeIn');
+      $('#left-circle').addClass('veristride');
+      $('#center-circle').addClass('datamurallogo');
+    }
+  )
+
+  $('#businessprocesses').click(
+    function() {
+      $('.project-circle').removeClass('hide-circle');
+      $('.project-circle').addClass('animated fadeIn');
+    }
+  )
+
+  $('#dataintegration').click(
+    function() {
+
+      $('.project-circle').removeClass('hide-circle');
+
+      $('.project-circle').addClass('animated fadeIn');
+    }
+  )
+
+  $('#mobileandenterprise').click(
+    function() {
+
+      $('.project-circle').removeClass('hide-circle');
+
+      $('.project-circle').addClass('animated fadeIn');
+    }
+  )
+
+});
