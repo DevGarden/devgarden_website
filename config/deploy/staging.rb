85fbdb0e4272e16f47b055fddcb57262814a3ea2
Darren Hicks <darren.hicks@gmail.com>
2016-06-29 19:54:00 -0600

    adding roles web and db

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
-server '52.3.179.157', user: 'devgarden_website', roles: %w{app}, "DOMAIN_NAME": "staging.devgarden.io"
+server '52.3.179.157', user: 'devgarden_website', roles: %w{app web db}, "DOMAIN_NAME": "staging.devgarden.io"
 set :linked_files, %w{config/secrets.yml}
